bb389cea8273a1c5406ec8d99e54a917f9793ad6
Manuel Garcia Genta <man.mdp@gmail.com>
2023-04-21 18:19:21 -0300

    section numbers

--- components/sections/contact/index.js
+++ components/sections/contact/index.js
@@ -1,15 +1,14 @@
-import Section from '@/components/section'
-import s from './contact.module.scss'
-import Link from 'next/link'
+import Section from "@/components/section"
+import s from "./contact.module.scss"
+import Link from "next/link"
 
 const Contact = () => {
   return (
-    <Section id="contact" className={s['contact-section']}>
+    <Section id="contact" className={s["contact-section"]}>
+      <span>03.</span>
       <h2 className={s.title}>Contact me.</h2>
       <article className={s.description}>
-        <p>
-          Let's work together to build something amazing!
-        </p>
+        <p>Let's work together to build something amazing!</p>
         <p>
           <Link href="mailto:man.mdp@gmail.com" className={s.email}>
             man.mdp@gmail.com
--- components/sections/what/index.js
+++ components/sections/what/index.js
@@ -1,16 +1,18 @@
-import Section from '@/components/section'
-import s from './what.module.scss'
-import { useYearsSince } from '@/hooks/use-years-since'
+import Section from "@/components/section"
+import s from "./what.module.scss"
+import { useYearsSince } from "@/hooks/use-years-since"
 
 const What = () => {
-  const years = useYearsSince('2014-12-01')
+  const years = useYearsSince("2014-12-01")
 
   return (
-    <Section id="what" className={s['what-section']}>
+    <Section id="what" className={s["what-section"]}>
+      <span>02.</span>
       <h2 className={s.title}>What I do?</h2>
       <article className={s.description}>
         <p>
-          I have over <span>{years}+ years</span> of experience working as a developer.
+          I have over <span>{years}+ years</span> of experience working as a
+          developer.
         </p>
       </article>
     </Section>
