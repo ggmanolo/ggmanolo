a7b2ae3617e6f46be230824b71b7e8d79167922e
Manuel Garcia Genta <man.mdp@gmail.com>
2023-05-10 02:05:45 -0300

    fonts/google and hero animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,35 @@
 import PropTypes from "prop-types"
+import { Roboto, Yellowtail, Orbitron } from "next/font/google"
+import clsx from "clsx"
+
 import "@/styles/globals.scss"
-import "../public/fonts/game-over.css"
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap"
+})
+
+const yellowtail = Yellowtail({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-yellowtail"
+})
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  variable: "--font-orbitron"
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={clsx(roboto.className, yellowtail.variable, orbitron.variable)}
+    >
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 App.propTypes = {
