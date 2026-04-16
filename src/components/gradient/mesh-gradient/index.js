504b57701b162daf1b5733918189fb9411498f52
Manuel Garcia Genta <man.mdp@gmail.com>
2026-04-16 12:45:45 -0300

    fix(mesh-gradient): remove implicit prop-types dep and invalid style={null}

--- src/components/gradient/mesh-gradient/index.js
+++ src/components/gradient/mesh-gradient/index.js
@@ -1,6 +1,5 @@
 "use client"
 import { useEffect } from "react"
-import PropTypes from "prop-types"
 import clsx from "clsx"
 import s from "./mesh-gradient.module.scss"
 
@@ -16,15 +15,10 @@ const MeshGradient = ({ className }) => {
     <canvas
       id="gradient-canvas"
       className={clsx(className, s.base, s.canvas)}
-      style={null}
       data-js-darken-top
       data-transition-in
     />
   )
-}
-
-MeshGradient.propTypes = {
-  className: PropTypes.string
 }
 
 export default MeshGradient
